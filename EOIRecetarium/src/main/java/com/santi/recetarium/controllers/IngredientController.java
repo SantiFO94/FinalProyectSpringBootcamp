97b4391fb7f50f9fecbdc4609ecc75021591629f
YagoFO94 <sfero15@gmail.com>
2021-04-12 19:48:37 +0200

    Arreglado el metodo getIngredient para que devuelva un objeto ResponseIngredient y no un Ingredient directamente. Añadidos los controles de error de todos los métodos y cambiados los nombres de las variables de las respuestas para que sean más explicativos (responseError y responseIngredient).

--- EOIRecetarium/src/main/java/com/santi/recetarium/controllers/IngredientController.java
+++ EOIRecetarium/src/main/java/com/santi/recetarium/controllers/IngredientController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ import com.santi.recetarium.entity.Ingredient;
 import com.santi.recetarium.entity.dto.IngredientDTOCard;
 import com.santi.recetarium.entity.response.ResponseIngredient;
 import com.santi.recetarium.entity.response.ResponseIngredients;
+import com.santi.recetarium.entity.response.ResponseIngredientsDTOCard;
 import com.santi.recetarium.services.IIngredientServiceIMPL;
 
 
@@ -38,22 +40,23 @@ public class IngredientController {
 	public ResponseEntity<?> getAllIngredients(){
 		
 		List<Ingredient> ingredients = new ArrayList<Ingredient>();
-		Map<String, Object> respuesta = new HashMap();
+		Map<String, Object> responseError = new HashMap();
+		
 		try {
 			ingredients = ingredientService.findAll();
 		}catch (DataAccessException e) { 
-			respuesta.put("mensaje", "Error al realizar la consulta de la base de datos");
-			respuesta.put("error", e.getMessage().concat(" ").concat(e.getMostSpecificCause().getMessage()));
-			return new ResponseEntity<Map<String, Object>>(respuesta, HttpStatus.INTERNAL_SERVER_ERROR);
+			responseError.put("mensaje", "Error al realizar la consulta de la base de datos");
+			responseError.put("error", e.getMessage().concat(" ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(responseError, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		if(ingredients.size()==0) {
-			respuesta.put("mensaje", "No existen ingredientes");
-			return new ResponseEntity<Map<String, Object>>(respuesta, HttpStatus.NOT_FOUND);
+			responseError.put("mensaje", "No existen ingredientes");
+			return new ResponseEntity<Map<String, Object>>(responseError, HttpStatus.NOT_FOUND);
 		}
 		
-		ResponseIngredients response = new ResponseIngredients(ingredients);
+		ResponseIngredients responseIngredient = new ResponseIngredients(ingredients);
 		
-		return new ResponseEntity<ResponseIngredients>(response, HttpStatus.OK);
+		return new ResponseEntity<ResponseIngredients>(responseIngredient, HttpStatus.OK);
 	}
 	
 	//sacar nombres de ingredientes para presentar en la versión resumida
@@ -61,45 +64,49 @@ public class IngredientController {
 	public ResponseEntity<?> getIngredientsNames(){
 		
 		List<Ingredient> ingredients = new ArrayList<Ingredient>();
-		Map<String, Object> respuesta = new HashMap();
+		Map<String, Object> responseError = new HashMap();
+		
 		try {
 			ingredients = ingredientService.findAll();
 		}catch (DataAccessException e) { 
-			respuesta.put("mensaje", "Error al realizar la consulta de la base de datos");
-			respuesta.put("error", e.getMessage().concat(" ").concat(e.getMostSpecificCause().getMessage()));
-			return new ResponseEntity<Map<String, Object>>(respuesta, HttpStatus.INTERNAL_SERVER_ERROR);
+			responseError.put("mensaje", "Error al realizar la consulta de la base de datos");
+			responseError.put("error", e.getMessage().concat(" ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(responseError, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		if(ingredients.size()==0) {
-			respuesta.put("mensaje", "No existen ingredientes");
-			return new ResponseEntity<Map<String, Object>>(respuesta, HttpStatus.NOT_FOUND);
+			responseError.put("mensaje", "No existen ingredientes");
+			return new ResponseEntity<Map<String, Object>>(responseError, HttpStatus.NOT_FOUND);
 		}
 		
 		List<IngredientDTOCard> ingredientsCard = new ArrayList<IngredientDTOCard>();
 		ingredients.forEach(i-> ingredientsCard.add(new IngredientDTOCard(i)));
-		ResponseIngredients response = new ResponseIngredients(ingredients);
+		ResponseIngredientsDTOCard responseIngredient = new ResponseIngredientsDTOCard(ingredientsCard);
 		
-		return new ResponseEntity<ResponseIngredients>(response, HttpStatus.OK);
+		return new ResponseEntity<ResponseIngredientsDTOCard>(responseIngredient, HttpStatus.OK);
 	}
 	
 	//encontrar un ingrediente completo en la receta (cuando se menciona para ver las cantidades) 
-	@GetMapping("/get/{id}")
+	@GetMapping("/ingredient/{id}")
 	public ResponseEntity<?> getIngredient(@PathVariable Long id){
 
 		Ingredient ingredient = null;
-		Map<String, Object> respuesta = new HashMap();
+		Map<String, Object> responseError = new HashMap();
+		
 		try {
 			ingredient = ingredientService.findById(id);
 		}catch (DataAccessException e) { 
-			respuesta.put("mensaje", "Error al realizar la consulta de la base de datos");
-			respuesta.put("error", e.getMessage().concat(" ").concat(e.getMostSpecificCause().getMessage()));
-			return new ResponseEntity<Map<String, Object>>(respuesta, HttpStatus.INTERNAL_SERVER_ERROR);
+			responseError.put("mensaje", "Error al realizar la consulta de la base de datos");
+			responseError.put("error", e.getMessage().concat(" ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(responseError, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		if(ingredient==null) {
-			respuesta.put("mensaje", "El identificador buscado no existe");
-			return new ResponseEntity<Map<String, Object>>(respuesta, HttpStatus.NOT_FOUND);
+			responseError.put("mensaje", "El identificador buscado no existe");
+			return new ResponseEntity<Map<String, Object>>(responseError, HttpStatus.NOT_FOUND);
 		}
 		
-		return new ResponseEntity<Ingredient>(ingredient, HttpStatus.OK);
+		ResponseIngredient responseIngredient = new ResponseIngredient(ingredient);
+		
+		return new ResponseEntity<ResponseIngredient>(responseIngredient, HttpStatus.OK);
 	}
 	
 	//agregar validaciones con @Valid y BindingResult result
@@ -107,59 +114,73 @@ public class IngredientController {
 	public ResponseEntity<?> addIngredient(@RequestBody Ingredient ingredient){
 		
 		Ingredient newIngredient = null;
-		Map<String, Object> respuesta = new HashMap();
+		Map<String, Object> responseError = new HashMap();
 		
 		try {
 			newIngredient = ingredientService.save(ingredient);
 		}catch(DataAccessException e) {
-			respuesta.put("mensaje", "Error al intentar insertar en la base de datos");
-			respuesta.put("error", e.getMessage().concat(" ").concat(e.getMostSpecificCause().getMessage()));
-			return new ResponseEntity<Map<String, Object>>(respuesta, HttpStatus.INTERNAL_SERVER_ERROR);
+			responseError.put("mensaje", "Error al intentar insertar en la base de datos");
+			responseError.put("error", e.getMessage().concat(" ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(responseError, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
-		ResponseIngredient response = new ResponseIngredient(newIngredient);
+		ResponseIngredient responseIngredient = new ResponseIngredient(newIngredient);
 
-		return new ResponseEntity<ResponseIngredient>(response,HttpStatus.CREATED);
+		return new ResponseEntity<ResponseIngredient>(responseIngredient,HttpStatus.CREATED);
 	}
 	
-	//terminar
-	@GetMapping("/update/{id}")
+	
+	@PutMapping("/update/{id}")
 	public ResponseEntity<?> updateIngredient(@RequestBody Ingredient ingredient, @PathVariable Long id){
 
-		Ingredient ingredientOriginal = ingredientService.findById(id);
+		Ingredient ingredientOriginal = null;
 		Ingredient ingredientUpdated = null;
-		Map<String, Object> respuesta = new HashMap();
+		Map<String, Object> responseError = new HashMap();
 		
 		try {
-			
+			ingredientOriginal = ingredientService.findById(id);
 		}catch (DataAccessException e) { 
-			respuesta.put("mensaje", "Error al realizar la consulta de la base de datos");
-			respuesta.put("error", e.getMessage().concat(" ").concat(e.getMostSpecificCause().getMessage()));
-			return new ResponseEntity<Map<String, Object>>(respuesta, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		if(ingredient==null) {
-			respuesta.put("mensaje", "El identificador buscado no existe");
-			return new ResponseEntity<Map<String, Object>>(respuesta, HttpStatus.NOT_FOUND);
+			responseError.put("mensaje", "Error al recuperar el ingrediente de la base de datos");
+			responseError.put("error", e.getMessage().concat(" ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(responseError, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
-		return new ResponseEntity<Ingredient>(ingredient, HttpStatus.OK);
+		if(ingredient==null) {
+			responseError.put("mensaje", "El identificador buscado no existe");
+			return new ResponseEntity<Map<String, Object>>(responseError, HttpStatus.NOT_FOUND);
+		}
+		
+		try {
+			ingredientOriginal.setName(ingredient.getName());
+			ingredientOriginal.setQuantity(ingredient.getQuantity());
+			ingredientOriginal.setMeasure(ingredient.getMeasure());
+			ingredientUpdated = ingredientService.save(ingredientOriginal);
+		}catch (DataAccessException e) { 
+			responseError.put("mensaje", "Error al actualizar el ingrediente en la base de datos");
+			responseError.put("error", e.getMessage().concat(" ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(responseError, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		ResponseIngredient responseIngredient = new ResponseIngredient(ingredientUpdated);
+		
+		return new ResponseEntity<ResponseIngredient>(responseIngredient, HttpStatus.OK);
 	}
 	
 	@DeleteMapping("/delete/{id}")
 	public ResponseEntity<?> deleteIngredient(@PathVariable Long id){
 		
-		Map<String, Object> respuesta = new HashMap();
+		Map<String, Object> responseError = new HashMap();
 		
 		try {
 			ingredientService.deleteById(id);
-		}catch (DataAccessException e) { //Base de datos inaccesible
-			respuesta.put("mensaje", "Error al intentar borrar de la base de datos");
-			respuesta.put("error", e.getMessage().concat(" ").concat(e.getMostSpecificCause().getMessage()));
-			return new ResponseEntity<Map<String, Object>>(respuesta, HttpStatus.INTERNAL_SERVER_ERROR);
+		}catch (DataAccessException e) {
+			responseError.put("mensaje", "Error al intentar borrar de la base de datos");
+			responseError.put("error", e.getMessage().concat(" ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(responseError, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
-		respuesta.put("mensaje","El ingrediente con id " + id + " ha sido borrado con éxito");
+		responseError.put("mensaje","El ingrediente con id " + id + " ha sido borrado con éxito");
 
-		return new ResponseEntity<>(HttpStatus.OK);
+		return new ResponseEntity<Map<String, Object>>(responseError, HttpStatus.OK);
 	}
 }
